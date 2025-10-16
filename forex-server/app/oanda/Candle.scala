a78e364545454b59d2071166f28b3fadbd311f81
Ethan Pedersen <45214966+ethanp55@users.noreply.github.com>
2025-10-16 11:26:23 -0600

    Add json capabilities for the candles that are returned from oanda

--- forex-server/app/oanda/Candle.scala
+++ forex-server/app/oanda/Candle.scala
@@ -1,10 +1,58 @@
 package oanda
 
-case class Candle(complete: Boolean,
-                  volume: Int,
-                  time: Double,
-                  bid: Option[Price],
-                  mid: Option[Price],
-                  ask: Option[Price])
+import play.api.libs.json.{JsObject, JsResult, JsValue, Json, OFormat}
+import java.time.{Instant, LocalDateTime, ZoneId}
 
-case class Price(o: Double, h: Double, l: Double, c: Double)
+case class Candle(
+    complete: Boolean,
+    volume: Int,
+    time: LocalDateTime,
+    bid: Option[Price],
+    mid: Option[Price],
+    ask: Option[Price]
+)
+
+object Candle {
+  implicit val candleFormat: OFormat[Candle] = new OFormat[Candle] {
+    override def writes(o: Candle): JsObject = Json.obj(
+      "complete" -> o.complete,
+      "volume" -> o.volume,
+      "time" -> o.time,
+      "bid" -> o.bid,
+      "mid" -> o.mid,
+      "ask" -> o.ask
+    )
+
+    def reads(json: JsValue): JsResult[Candle] = for {
+      complete <- (json \ "complete").validate[Boolean]
+      volume <- (json \ "volume").validate[Int]
+      time <- (json \ "time").validate[String].map { x =>
+        val instant = Instant.ofEpochSecond(x.toDouble.toLong)
+        LocalDateTime.ofInstant(instant, ZoneId.of("UTC"))
+      }
+      bid <- (json \ "bid").validateOpt[Price]
+      mid <- (json \ "mid").validateOpt[Price]
+      ask <- (json \ "ask").validateOpt[Price]
+    } yield Candle(complete, volume, time, bid, mid, ask)
+  }
+}
+
+case class Price(o: Float, h: Float, l: Float, c: Float)
+
+object Price {
+  implicit val priceFormat: OFormat[Price] = new OFormat[Price] {
+    override def writes(o: Price): JsObject = Json.obj(
+      "o" -> o.o,
+      "h" -> o.h,
+      "l" -> o.l,
+      "c" -> o.c
+    )
+
+    override def reads(json: JsValue): JsResult[Price] = for {
+      o <- (json \ "o").validate[String].map(_.toFloat)
+      h <- (json \ "h").validate[String].map(_.toFloat)
+      l <- (json \ "l").validate[String].map(_.toFloat)
+      c <- (json \ "c").validate[String].map(_.toFloat)
+    } yield Price(o, h, l, c)
+  }
+}
--- forex-server/app/oanda/DataDownloader.scala
+++ forex-server/app/oanda/DataDownloader.scala
@@ -3,33 +3,61 @@ package oanda
 import sttp.client4.quick.*
 import sttp.client4.{Request, Response}
 import play.api.libs.json.*
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDateTime, ZoneOffset}
 
 object DataDownloader {
   private val baseUrl: String =
     "https://api-fxpractice.oanda.com/v3/instruments"
-  private val currencyPair: String = "EUR_USD"
-  private val granularity: String = "H1"
-  private val price: String = "M"
-  val apiToken: String = Config.apiToken
+
   private val dateFormat: String = "UNIX"
 
-  private val headers = Map(
-    "price" -> price,
-    "granularity" -> granularity,
-    "count" -> "1000"
-  )
-  val uri = uri"$baseUrl/$currencyPair/candles?$headers"
+  private val apiToken: String = Config.apiToken
 
-  val request: Request[String] = quickRequest
-    .get(uri)
-    .auth
-    .bearer(apiToken)
-    .header("Accept-Datetime-Format", dateFormat)
-    .header("Content-Type", "application/json")
-  val response: Response[String] = request.send()
+  def dateTimeToUnix(localDateTime: LocalDateTime): String =
+    localDateTime.toEpochSecond(ZoneOffset.UTC).toString
+
+  def downloadCandles(): Seq[Candle] = {
+    val currencyPair: String = "EUR_USD"
+    val granularity: String = "H1"
+    val price: String = "M"
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    val fromDate = LocalDateTime.parse("2025-01-01 00:00:00", formatter)
+    val toDate = LocalDateTime.parse("2025-05-01 00:00:00", formatter)
+
+    val headers = Map(
+      "price" -> price,
+      "granularity" -> granularity,
+      "from" -> DataDownloader.dateTimeToUnix(fromDate),
+      "to" -> DataDownloader.dateTimeToUnix(toDate)
+    )
+    val uri = uri"$baseUrl/$currencyPair/candles?$headers"
+
+    val request: Request[String] = quickRequest
+      .get(uri)
+      .auth
+      .bearer(apiToken)
+      .header("Accept-Datetime-Format", dateFormat)
+      .header("Content-Type", "application/json")
+    val response: Response[String] = request.send()
+    val candlesJson: JsValue = Json.parse(response.body)("candles")
+    candlesJson.as[Seq[Candle]]
+  }
 }
 
 @main
 def main(): Unit = {
-  println(DataDownloader.response)
+  println(DataDownloader.downloadCandles())
 }
+
+/*
+ * TODO:
+  - Date range (to, from, iteratively build)
+  - Enums for pair, granularity, and prices (b/m/a)
+  - Request to controller
+  - Update data downloader to use candle requests (prev step)
+  - Error handling
+  - Controller
+  - Unit tests
+  - Controller tests
+ * */
