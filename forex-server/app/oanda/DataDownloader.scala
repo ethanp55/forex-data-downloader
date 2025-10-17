40f0e61d8f478c4008cedc4208200ff1b8c7dd91
Ethan Pedersen <45214966+ethanp55@users.noreply.github.com>
2025-10-17 10:19:51 -0600

    Add timeout error handling

--- forex-server/app/oanda/DataDownloader.scala
+++ forex-server/app/oanda/DataDownloader.scala
@@ -1,22 +1,26 @@
 package oanda
 
 import sttp.client4.quick.*
-import sttp.client4.{Request, Response}
+import sttp.client4.Request
 import play.api.libs.json.*
 import request.{
   Ask,
   Bid,
   CandlesDownloadRequest,
   EUR_USD,
-  H1,
+  M1,
   Mid,
   PricingComponent
 }
 import sttp.model.StatusCode
-
 import java.time.format.{DateTimeFormatter, DateTimeParseException}
 import java.time.{LocalDateTime, ZoneOffset}
+import java.util.concurrent.{ExecutorService, Executors, TimeoutException}
 import scala.annotation.tailrec
+import scala.concurrent.duration.*
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.boundary
+import scala.util.boundary.{Label, break}
 
 object DataDownloader {
   private val baseUrl: String =
@@ -32,39 +36,73 @@ object DataDownloader {
     StatusCode.ServiceUnavailable,
     StatusCode.GatewayTimeout
   )
+  // Oanda's API only sends a max of 5000 candles per request
+  private val maxCandlesPerRequest: Int = 5000
+  private val threadPool: ExecutorService = Executors.newFixedThreadPool(20)
+  private implicit val executionContext: ExecutionContext =
+    ExecutionContext.fromExecutor(threadPool)
+  private val waitTime: Int = 20
 
   private def dateTimeToUnix(localDateTime: LocalDateTime): String =
     localDateTime.toEpochSecond(ZoneOffset.UTC).toString
 
-  private def createRequest(
+  private def createRequests(
       candlesDownloadRequest: CandlesDownloadRequest
-  ): Either[Error, Request[String]] = {
+  ): Either[Error, Seq[Request[String]]] = {
     try {
       val fromDate =
         LocalDateTime.parse(candlesDownloadRequest.fromDate, formatter)
       val toDate = LocalDateTime.parse(candlesDownloadRequest.toDate, formatter)
-      val headers = Map(
-        "price" -> PricingComponent.combine(
-          candlesDownloadRequest.pricingComponents
-        ),
-        "granularity" -> candlesDownloadRequest.granularity.toString,
-        "from" -> DataDownloader.dateTimeToUnix(fromDate),
-        "to" -> DataDownloader.dateTimeToUnix(toDate)
-      )
-      val uri =
-        uri"$baseUrl/${candlesDownloadRequest.currencyPair.toString}/candles?$headers"
+      val numMinutesPerRange: Long =
+        maxCandlesPerRequest * candlesDownloadRequest.granularity.toMinutes
 
-      val request: Request[String] = quickRequest
-        .get(uri)
-        .auth
-        .bearer(apiToken)
-        .header("Accept-Datetime-Format", dateFormat)
-        .header("Content-Type", "application/json")
-      Right(request)
+      @tailrec
+      def _createDateRanges(
+          currentFromDate: LocalDateTime,
+          dateRanges: Seq[(LocalDateTime, LocalDateTime)]
+      ): Seq[(LocalDateTime, LocalDateTime)] = {
+        if (currentFromDate.isBefore(toDate)) {
+          val newToDate = currentFromDate.plusMinutes(numMinutesPerRange)
+          val currentToDate =
+            if (newToDate.isBefore(toDate)) newToDate else toDate
+
+          _createDateRanges(
+            currentToDate,
+            dateRanges :+ (currentFromDate, currentToDate)
+          )
+        } else {
+          dateRanges
+        }
+      }
+
+      val dateRanges = _createDateRanges(fromDate, Seq.empty)
+      val requests = dateRanges.map { (currFromTime, currToTime) =>
+        val headers = Map(
+          "price" -> PricingComponent.combine(
+            candlesDownloadRequest.pricingComponents
+          ),
+          "granularity" -> candlesDownloadRequest.granularity.toString,
+          "from" -> DataDownloader.dateTimeToUnix(currFromTime),
+          "to" -> DataDownloader.dateTimeToUnix(currToTime)
+        )
+
+        val uri =
+          uri"$baseUrl/${candlesDownloadRequest.currencyPair.toString}/candles?$headers"
+
+        quickRequest
+          .get(uri)
+          .auth
+          .bearer(apiToken)
+          .header("Accept-Datetime-Format", dateFormat)
+          .header("Content-Type", "application/json")
+      }
+
+      Right(requests)
     } catch {
       case _: DateTimeParseException =>
         val requestedFromDate = candlesDownloadRequest.fromDate
         val requestedToDate = candlesDownloadRequest.toDate
+
         Left(
           DateTimeParseError(
             s"Could not convert $requestedFromDate and/or $requestedToDate to ${formatter.toString}"
@@ -73,29 +111,65 @@ object DataDownloader {
     }
   }
 
-  @tailrec
-  private def sendRequest(
-      request: Request[String],
-      numRetries: Int = nRetries
+  private def sendRequests(
+      requests: Seq[Request[String]]
   ): Either[Error, Seq[Candle]] = {
-    val response = request.send()
-    response.code match {
-      case StatusCode.Ok =>
-        val candlesJson: JsValue = Json.parse(response.body)("candles")
-        val candles = candlesJson.as[Seq[Candle]]
-        Right(candles)
-      case x if retryStatusCodes.contains(x) && numRetries > 0 =>
-        Thread.sleep(nSecondsBetweenRetries * 1000)
-        sendRequest(request, numRetries - 1)
-      case _ => Left(OandaApiError(response.body, response.code.toString))
+    @tailrec
+    def _sendWithRetry(
+        request: Request[String],
+        numRetries: Int
+    ): Either[Error, Seq[Candle]] = {
+      val response = request.send()
+
+      response.code match {
+        case StatusCode.Ok =>
+          val candlesJson: JsValue = Json.parse(response.body)("candles")
+          val candles = candlesJson.as[Seq[Candle]]
+
+          Right(candles)
+
+        case x if retryStatusCodes.contains(x) && numRetries > 0 =>
+          Thread.sleep(nSecondsBetweenRetries * 1000)
+          _sendWithRetry(request, numRetries - 1)
+
+        case _ => Left(OandaApiError(response.body, response.code.toString))
+      }
+    }
+
+    val requestFutures =
+      Future.sequence(
+        requests.map(request => Future(_sendWithRetry(request, nRetries)))
+      )
+
+    try {
+      val results = Await.result(requestFutures, waitTime.seconds)
+      implicit val boundaryLabel: Label[Error] = new Label[Error]
+      val candleSequences: Either[Error, Seq[Candle]] = boundary {
+        val candles = results.collect {
+          case Left(error)    => break(error)
+          case Right(candles) => candles
+        }
+
+        Right(candles.flatten)
+      }
+
+      candleSequences
+    } catch {
+      case _: TimeoutException =>
+        Left(
+          OandaApiError(
+            s"Timeout to Oanda API with URI ${requests.head.uri}",
+            StatusCode.RequestTimeout.toString
+          )
+        )
     }
   }
 
   def downloadCandles(
       candlesDownloadRequest: CandlesDownloadRequest
   ): Either[Error, Seq[Candle]] = {
-    val requestEither = createRequest(candlesDownloadRequest)
-    requestEither.flatMap(sendRequest(_))
+    val requestsEither = createRequests(candlesDownloadRequest)
+    requestsEither.flatMap(sendRequests)
   }
 }
 
@@ -103,17 +177,16 @@ object DataDownloader {
 def main(): Unit = {
   val request = CandlesDownloadRequest(
     EUR_USD,
-    H1,
+    M1,
     Seq(Ask, Bid, Mid),
-    "2025-01-01 00:00:00",
-    "2025-05-01 00:00:00"
+    "2024-01-01 00:00:00",
+    "2025-10-01 00:00:00"
   )
   println(DataDownloader.downloadCandles(request))
 }
 
 /*
  * TODO:
-  - Date range (to, from, iteratively build)
   - Controller
   - Unit tests
   - Controller tests
