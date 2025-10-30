2fe1a285311ac22c7718b7f4e66e20e95d90dc3a
Ethan Pedersen <45214966+ethanp55@users.noreply.github.com>
2025-10-30 13:05:29 -0600

    Add validation, improve formatting

--- forex-frontend/src/app/downloader/downloader.ts
+++ forex-frontend/src/app/downloader/downloader.ts
@@ -2,10 +2,17 @@ import { Component } from "@angular/core";
 import { CurrencyPair } from "./request/currency-pair.enums";
 import { Granularity } from "./request/granularity.enums";
 import { PricingComponent } from "./request/pricing-component.enums";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {
+    FormBuilder,
+    Validators,
+    FormsModule,
+    ReactiveFormsModule,
+    FormGroup,
+} from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatTooltipModule } from "@angular/material/tooltip";
 
 @Component({
     selector: "app-downloader",
@@ -15,37 +22,64 @@ import { MatFormFieldModule } from "@angular/material/form-field";
         CommonModule,
         MatDatepickerModule,
         MatFormFieldModule,
+        MatTooltipModule,
     ],
     templateUrl: "./downloader.html",
     styleUrl: "./downloader.css",
 })
 export class Downloader {
-    // Options
+    form: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+        // Set up the various inputs, with checks
+        this.form = this.formBuilder.group(
+            {
+                currencyPair: [null, Validators.required],
+                granularity: [null, Validators.required],
+                pricingComponents: [[], Validators.required],
+                startDate: [null, Validators.required],
+                endDate: [null, Validators.required],
+            },
+            { validators: this.dateRangeValidator },
+        );
+    }
+
+    // Options that users can choose from
     protected readonly currencyPairOptions = CurrencyPair;
     protected readonly granularityOptions = Granularity;
     protected readonly pricingComponentOptions = PricingComponent;
 
-    // Selected values
-    protected currencyPair?: CurrencyPair = undefined;
-    protected granularity?: Granularity = undefined;
-    protected pricingComponents: PricingComponent[] = [];
-    protected startDate?: Date = undefined;
-    protected endDate?: Date = undefined;
-
+    // Used for updating the array of price options (bid, mid, and/or ask)
     public togglePricingOption(option: PricingComponent): void {
-        const index = this.pricingComponents.indexOf(option);
+        const pricingComponents = this.form.get("pricingComponents")?.value;
 
-        if (index > -1) {
-            this.pricingComponents.splice(index, 1);
+        if (pricingComponents.includes(option)) {
+            this.form.patchValue({
+                pricingComponents: pricingComponents.filter(
+                    (item: PricingComponent) => item !== option,
+                ),
+            });
         } else {
-            this.pricingComponents.push(option);
+            pricingComponents.push(option);
+            this.form.patchValue({ pricingComponents: pricingComponents });
+        }
+    }
+
+    // Make sure the start date is before the end date (and that they're both defined)
+    private dateRangeValidator(formGroup: FormGroup): { [key: string]: boolean } | null {
+        const startDate = formGroup.get("startDate")?.value;
+        const endDate = formGroup.get("endDate")?.value;
+
+        return startDate && endDate && startDate > endDate ? { dateMismatch: true } : null;
+    }
+
+    // Create and send the download request
+    downloadCandles() {
+        if (this.form.valid) {
+            console.log("submit");
         }
     }
 }
 
 // TODO:
-// - Add validation
-// - Add submit button
-// - Only enable submit button when the data is clean and ready to go
-// - Change tooltip to only show on hover
 // - Create service to send requests and receive responses from backend
