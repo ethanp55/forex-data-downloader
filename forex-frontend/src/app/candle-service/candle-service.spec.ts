8e1e75da1dbe5b7054e1888fdcf7839e983e6c20
Ethan Pedersen <45214966+ethanp55@users.noreply.github.com>
2025-11-13 10:18:05 -0700

    Add candle service tests

--- forex-frontend/src/app/candle-service/candle-service.spec.ts
+++ forex-frontend/src/app/candle-service/candle-service.spec.ts
@@ -1,16 +1,167 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { CandleService } from "./candle-service";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { CandlesDownloadRequest } from "../downloader/request/candles-download-request";
+import { of, throwError } from "rxjs";
+import { CurrencyPair } from "../downloader/request/currency-pair.enums";
+import { Granularity } from "../downloader/request/granularity.enums";
+import { PricingComponent } from "../downloader/request/pricing-component.enums";
+import { Candle } from "../downloader/response/candle";
 
-import { CandleService } from './candle-service';
+describe("CandleService", () => {
+    let httpClientSpy: jasmine.SpyObj<HttpClient>;
+    let service: CandleService;
+    let mockDownloadRequest: CandlesDownloadRequest;
 
-describe('CandleService', () => {
-  let service: CandleService;
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj("HttpClient", ["post"]);
+        TestBed.configureTestingModule({
+            providers: [CandleService, { provide: HttpClient, useValue: httpClientSpy }],
+        });
+        service = TestBed.inject(CandleService);
+        mockDownloadRequest = new CandlesDownloadRequest(
+            CurrencyPair.AUD_JPY,
+            Granularity.H8,
+            [PricingComponent.Bid, PricingComponent.Ask],
+            "2025-09-07T00:00:00Z",
+            "2025-10-07T00:00:00Z"
+        );
+    });
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CandleService);
-  });
+    it("should be created", () => {
+        expect(service).toBeTruthy();
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    describe("should properly handle errors", () => {
+        it("with no missing fields", () => {
+            const errorResponse = {
+                status: 500,
+                error: JSON.stringify({ code: "SERVER_ERROR", message: "Internal Server Error" }),
+            };
+            httpClientSpy.post.and.returnValue(throwError(() => errorResponse));
+
+            service.downloadCandles(mockDownloadRequest);
+
+            const candles = service.candlesSignal();
+            expect(candles.length).toBe(0);
+
+            const errorMessage = service.errorMessageSignal();
+            expect(errorMessage).toBeDefined();
+            expect(errorMessage).toBe(
+                "status = 500; code = SERVER_ERROR; message = Internal Server Error"
+            );
+        });
+
+        it("with a missing error field", () => {
+            const errorResponse = { status: 500 };
+            httpClientSpy.post.and.returnValue(throwError(() => errorResponse));
+
+            service.downloadCandles(mockDownloadRequest);
+
+            const candles = service.candlesSignal();
+            expect(candles.length).toBe(0);
+
+            const errorMessage = service.errorMessageSignal();
+            expect(errorMessage).toBeDefined();
+            expect(errorMessage).toBe(
+                "status = 500; code = UNKNOWN_ERROR; message = (message not provided)"
+            );
+        });
+
+        it("with a missing status field", () => {
+            const errorResponse = {
+                error: JSON.stringify({ code: "SERVER_ERROR", message: "Internal Server Error" }),
+            };
+            httpClientSpy.post.and.returnValue(throwError(() => errorResponse));
+
+            service.downloadCandles(mockDownloadRequest);
+
+            const candles = service.candlesSignal();
+            expect(candles.length).toBe(0);
+
+            const errorMessage = service.errorMessageSignal();
+            expect(errorMessage).toBeDefined();
+            expect(errorMessage).toBe(
+                "status = 520; code = SERVER_ERROR; message = Internal Server Error"
+            );
+        });
+
+        it("with missing status and error fields", () => {
+            const errorResponse = {};
+            httpClientSpy.post.and.returnValue(throwError(() => errorResponse));
+
+            service.downloadCandles(mockDownloadRequest);
+
+            const candles = service.candlesSignal();
+            expect(candles.length).toBe(0);
+
+            const errorMessage = service.errorMessageSignal();
+            expect(errorMessage).toBeDefined();
+            expect(errorMessage).toBe(
+                "status = 520; code = UNKNOWN_ERROR; message = (message not provided)"
+            );
+        });
+    });
+
+    it("properly handle cases with no request errors, but where unexpected JSON was received from the server", () => {
+        const mockResponse = "foo bar baz wtf";
+        httpClientSpy.post.and.returnValue(of(new HttpResponse({ body: mockResponse })));
+
+        service.downloadCandles(mockDownloadRequest);
+
+        const candles = service.candlesSignal();
+        expect(candles.length).toBe(0);
+
+        const errorMessage = service.errorMessageSignal();
+        expect(errorMessage).toBeDefined();
+        expect(errorMessage).toBe(
+            "Unknown body data type (expected an array of candles): foo bar baz wtf"
+        );
+    });
+
+    describe("should properly download candles", () => {
+        it("when there is a single candle array returned from the server", () => {
+            const mockResponse =
+                '[{"complete": true, "volume": 100, "time": "1631136000", "bid": { "o": "1.1", "h": "1.2", "l": "1.0", "c": "1.15" }, "mid": { "o": "1.05", "h": "1.1", "l": "1.0", "c": "1.05" }, "ask": { "o": "1.2", "h": "1.25", "l": "1.15", "c": "1.2" }}]';
+            httpClientSpy.post.and.returnValue(of(new HttpResponse({ body: mockResponse })));
+
+            service.downloadCandles(mockDownloadRequest);
+
+            const errorMessage = service.errorMessageSignal();
+            expect(errorMessage).toBeNull();
+
+            const candles = service.candlesSignal();
+            expect(candles.length).toBe(1);
+            const expectedCandle = Candle.fromJSON(JSON.parse(mockResponse)[0]);
+            expect(candles[0]).toEqual(expectedCandle);
+        });
+
+        it("when there are multiple candle arrays returned from the server", () => {
+            const mockResponse =
+                '[{"complete": true, "volume": 100, "time": "1631136000", "bid": { "o": "1.1", "h": "1.2", "l": "1.0", "c": "1.15" }, "mid": { "o": "1.05", "h": "1.1", "l": "1.0", "c": "1.05" }, "ask": { "o": "1.2", "h": "1.25", "l": "1.15", "c": "1.2" }}],[{"complete": true, "volume": 100, "time": "1631136000", "bid": { "o": "1.1", "h": "1.2", "l": "1.0", "c": "1.15" }, "mid": { "o": "1.05", "h": "1.1", "l": "1.0", "c": "1.05" }, "ask": { "o": "1.2", "h": "1.25", "l": "1.15", "c": "1.2" }}]';
+            httpClientSpy.post.and.returnValue(of(new HttpResponse({ body: mockResponse })));
+
+            service.downloadCandles(mockDownloadRequest);
+
+            const errorMessage = service.errorMessageSignal();
+            expect(errorMessage).toBeNull();
+
+            const candles = service.candlesSignal();
+            expect(candles.length).toBe(2);
+        });
+
+        it("when there are multiple candle arrays returned from the server, each with different numbers of candles", () => {
+            const mockResponse =
+                '[{"complete": true, "volume": 100, "time": "1631136000", "bid": { "o": "1.1", "h": "1.2", "l": "1.0", "c": "1.15" }, "mid": { "o": "1.05", "h": "1.1", "l": "1.0", "c": "1.05" }, "ask": { "o": "1.2", "h": "1.25", "l": "1.15", "c": "1.2" }},{"complete": true, "volume": 100, "time": "1631136000", "bid": { "o": "1.1", "h": "1.2", "l": "1.0", "c": "1.15" }, "mid": { "o": "1.05", "h": "1.1", "l": "1.0", "c": "1.05" }, "ask": { "o": "1.2", "h": "1.25", "l": "1.15", "c": "1.2" }}],[{"complete": true, "volume": 100, "time": "1631136000", "bid": { "o": "1.1", "h": "1.2", "l": "1.0", "c": "1.15" }, "mid": { "o": "1.05", "h": "1.1", "l": "1.0", "c": "1.05" }, "ask": { "o": "1.2", "h": "1.25", "l": "1.15", "c": "1.2" }}]';
+            httpClientSpy.post.and.returnValue(of(new HttpResponse({ body: mockResponse })));
+
+            service.downloadCandles(mockDownloadRequest);
+
+            const errorMessage = service.errorMessageSignal();
+            expect(errorMessage).toBeNull();
+
+            const candles = service.candlesSignal();
+            expect(candles.length).toBe(3);
+        });
+    });
 });
