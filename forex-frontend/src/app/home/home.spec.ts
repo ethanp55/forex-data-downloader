7802a0ebb74aa09e116a2854668f3de97f577230
Ethan Pedersen <45214966+ethanp55@users.noreply.github.com>
2025-11-14 09:08:22 -0700

    Add home component tests

--- forex-frontend/src/app/home/home.spec.ts
+++ forex-frontend/src/app/home/home.spec.ts
@@ -16,7 +16,44 @@ describe("Home", () => {
         fixture.detectChanges();
     });
 
-    it("should create", () => {
+    it("should be created", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should have the correct Oanda URL", () => {
+        expect(component.oandaUrl).toBe("https://developer.oanda.com/rest-live-v20/instrument-ep/");
+    });
+
+    describe("text", () => {
+        it("should show the correct amount of paragraphs", () => {
+            const compiled = fixture.nativeElement as HTMLElement;
+
+            expect(compiled.querySelectorAll("p").length).toBe(4);
+        });
+
+        it("should be properly displayed", () => {
+            const compiled = fixture.nativeElement as HTMLElement;
+            const paragraphTags = compiled.querySelectorAll("p");
+
+            expect(paragraphTags[0].className).toBe("biggest");
+            expect(paragraphTags[0].textContent).toBe("Download historical forex data.");
+
+            expect(paragraphTags[1].className).toBe("big");
+            expect(paragraphTags[1].textContent).toBe(
+                "Enhance your analysis, backtesting, and trading strategies."
+            );
+
+            expect(paragraphTags[2].className).toBe("note");
+            expect(paragraphTags[2].textContent).toBe(
+                " Requests with more than 5000 candles might be slower and/or time out, as we are limited by Oanda's API. "
+            );
+            expect(paragraphTags[2].children[0].nodeName).toBe("A");
+            expect(paragraphTags[2].children[0].textContent).toBe("Oanda's API");
+
+            expect(paragraphTags[3].className).toBe("note");
+            expect(paragraphTags[3].textContent).toBe(
+                "Times are returned as UTC. Daily alignments are made at hour 0."
+            );
+        });
     });
 });
